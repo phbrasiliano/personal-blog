5c0330aef6671a3a143ec0e816fb9717f170016d
Phellipe <phbrasiliano@gmail.com>
2017-08-21 15:15:39 +0200

    allows the forms to correctly update the description

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.friendly.find(params[:id])
 
-    if @post.update(params[:post].permit(:title, :body))
+    if @post.update(params[:post].permit(:title, :body, :description))
       redirect_to @post
     else
       render 'edit'
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :description)
     end
 end
