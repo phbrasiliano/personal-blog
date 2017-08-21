1518e614800ceeb039f43a981ed117e430decbc0
Phellipe <phbrasiliano@gmail.com>
2017-08-21 02:17:14 +0200

    adds dynamic metadata tags to the website

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,7 @@
 module PostsHelper
+  # retrieves first photo from the post, if any
+  def retrieve_photo(post)
+    /<img.*?src=\"(.+?)\"/.match(post.body)[1] unless /<img.*?src=\"(.+?)\"/.match(post.body).nil?
+  end
+
 end
