7be8c2fcc0543caacfd084f9783ac09ba1572ebd
Phellipe <phbrasiliano@gmail.com>
2017-08-22 23:59:29 +0200

    adds pagination function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @posts = Post.all.order('created_at DESC')
+    @posts = Post.paginate(page: params[:page], per_page: 8)
   end
 
   def new
