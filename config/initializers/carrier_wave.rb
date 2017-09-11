c6c6b868a98a42bd88da828703a80f33e3348cc3
Phellipe <phbrasiliano@gmail.com>
2017-09-11 13:56:32 +0200

    removes AWS S3 bucket variables

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,4 +1,5 @@
 if Rails.env.production?
+=begin
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
@@ -9,4 +10,5 @@ if Rails.env.production?
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
+=end
 end
