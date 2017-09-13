84ad38cfe8d7f1423472a472d5e419277c0de236
Phellipe <phbrasiliano@gmail.com>
2017-09-13 14:43:04 +0200

    changes picture upload folder

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -50,10 +50,4 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   else
     storage :file
   end
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
 end
