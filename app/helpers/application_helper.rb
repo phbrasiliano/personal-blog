a426639262c8c405b7ff8fb0cf130f9c601c60e0
Phellipe <phbrasiliano@gmail.com>
2017-11-11 18:44:06 +0100

    Changes the way titles are displayed. Light refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,12 @@ module ApplicationHelper
 
   def full_title(page_title ='')
     base_title = "Groselha Factory"
-    if page_title.empty?
-      base_title
+    if page_title.empty? || page_title == "Groselha Factory"
+      base_title + " | " + "Blog"
     else
       page_title + " | " + base_title
     end
   end
+
+
 end
